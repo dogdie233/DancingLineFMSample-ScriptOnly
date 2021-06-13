573fbd8a9bee3f4cdbfd05f7fe54808402a1a6a4
dogdie233 <dogdie233@163.com>
2021-06-13 12:13:02 +0800

    v0.0.5
    Fix #6
    EventManager thread safe and change Invoke method
    Fixed some wrong class and variable names
    Restart support
    CameraFollower support pivotChange
    Crown support automode
    Skin system
    fix charset error?

---
+++ Scripts/AnimationController.cs
@@ -0,0 +1,40 @@
+﻿using Event;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace Level.Animations
+{
+    public class AnimationController : MonoBehaviour
+    {
+        public PlayableDirector timeline;
+
+        void Start()
+        {
+            EventManager.onStateChange.AddListener(args =>
+            {
+                switch (args.newState)
+                {
+                    case GameState.SelectingSkins:
+                        timeline.time = 0f;
+                        timeline.Stop();
+                        break;
+                    case GameState.Playing:
+                        timeline.Play();
+                        break;
+                    case GameState.WaitingRespawn:
+                    case GameState.GameOver:
+                        timeline.Pause();
+                        break;
+                }
+                return args;
+            }, Priority.Monitor);
+            EventManager.onRespawn.AddListener(args =>
+            {
+                timeline.time = args.crown.time;
+                return args;
+            }, Priority.Monitor);
+        }
+    }
+}
